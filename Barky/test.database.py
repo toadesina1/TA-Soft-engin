33624174b15c7e706ff1ee887357e66fbeba3574
toadesina1 <77985446+toadesina1@users.noreply.github.com>
2021-03-03 16:10:21 -0600

    Add files via upload

---
+++ Barky/test.database.py
@@ -0,0 +1,77 @@
+# the code is forked from Proffesor Jeff zoom tutoring
+
+import os
+from datetime import datetime
+import sqlite3
+
+import pytest
+
+
+from database import DatabaseManager
+
+@pytest.fixture
+def database_manager() -> DatabaseManager:
+    """
+    define the databasemanager from the database
+    """
+    filename = "test_bookmarks.db"
+    dbm = DatabaseManager(filename)
+    yield dbm
+    dbm.__del__()           # release the database manager
+    os.remove(filename)
+
+
+def test_database_manager_create_table(database_manager):
+    # arrange and act
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    #assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+
+    cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='bookmarks' ''')
+
+    assert cursor.fetchone()[0] == 1
+
+    #cleanup
+    database_manager.drop_table("bookmarks")
+
+
+def test_database_manager_add_bookmark(database_manager):
+
+    # arrange
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    data = {
+        "title": "test_title",
+        "url": "http://example.com",
+        "notes": "test notes",
+        "date_added": datetime.utcnow().isoformat()        
+    }
+
+    # act
+    database_manager.add("bookmarks", data)
+
+    # assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute(''' SELECT * FROM bookmarks WHERE title='test_title' ''')    
+    assert cursor.fetchone()[0] == 1    
