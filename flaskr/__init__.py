76e9dde30fac8b04396d980b0e9fe2cebd505cc7
Jeffry Babb <ahuimanu@gmail.com>
2021-03-06 23:46:52 -0600

    completed the blog blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,19 +7,20 @@ def create_app(test_config=None):
     From the "Patterns for Flask" section in the documentation, this is a factory pattern for Flask:
     https://flask.palletsprojects.com/en/1.1.x/patterns/appfactories/
     """
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        app.config.update(test_config)
 
     # ensure the instance folder exists
     try:
@@ -27,17 +28,29 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
 
-    # db init stuff
+    @app.route("/index")
+    def index():
+        return "where it all begins"
+
+    # register the database commands
     from . import db
+
     db.init_app(app)
 
-    # auth bp
+    # apply the blueprints to the app
     from . import auth
+
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
 
     return app
