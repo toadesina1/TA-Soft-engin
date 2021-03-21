e0a75813cc40cfb618efeacaaf4071d41ce66267
toadesina1 <77985446+toadesina1@users.noreply.github.com>
2021-03-20 21:33:35 -0500

    Add files via upload

---
+++ Barky Transform/flaskr/__init__.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+# https://pypi.org/project/python-dotenv/
+from dotenv import load_dotenv
+load_dotenv()
+# env_path = Path('.') / '.env'
+# load_dotenv(dotenv_path=env_path)
+
+
+# print (os.getenv('FLASK_APP'))
---
+++ Barky Transform/flaskr/schema.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE bookmarks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  );
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+   "title": "text not null",
+   "url": "text not null",
+   "notes": "text",
+   "date_added": "text not null",
+);
+
